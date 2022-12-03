4ae39e762e536b256d2274e045c01f57c4c3aa77
janirac <104847953+janirac@users.noreply.github.com>
2022-12-03 17:39:29 -0500

    Delete W2D3 directory

--- W2D3/battleship_project/lib/player.rb
+++
@@ -1,8 +0,0 @@
-class Player
-    def get_move
-        puts "enter a position with coordinates separated with a space like `4 7`"
-        get = gets.chomp
-        array = get.split(" ")
-        array.map { |ele| ele.to_i}
-    end 
-end
